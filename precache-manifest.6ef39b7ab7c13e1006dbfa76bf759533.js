f940450adbe85624b8e0208d52fda5793c45404d
julianaputra <julianaputra10@gmail.com>
2020-09-18 17:39:31 +0800

    Updates

---
+++ precache-manifest.6ef39b7ab7c13e1006dbfa76bf759533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afff3f5d7d3b765e19a674fc4c3adc64",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "da5ab0b7ad6b2708146a",
+    "url": "/react-calculator/static/css/main.96b26384.chunk.css"
+  },
+  {
+    "revision": "0a3c8bb5e58ffd8f5ee3",
+    "url": "/react-calculator/static/js/2.2c3ba907.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.2c3ba907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5ab0b7ad6b2708146a",
+    "url": "/react-calculator/static/js/main.6699faac.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
